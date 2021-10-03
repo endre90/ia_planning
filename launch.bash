1d4f122cacc0fc0fc7c1766afec3f8d259367251
Kristofer Bengtsson <kristofer@sekvensa.se>
2021-10-03 12:35:40 +0200

    Updated the rviz window size on launch

--- launch.bash
+++ launch.bash
@@ -3,4 +3,4 @@ killall light-locker
 
 xrandr --newmode "1600x960_60.00"  127.00  1600 1696 1864 2128  960 963 973 996 -hsync +vsync
 xrandr --addmode VNC-0 1600x960_60.00
-xrandr --size 1600x960
+xrandr --size 1280x960
